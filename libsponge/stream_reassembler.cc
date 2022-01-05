967cf8c0ea27ff2f7f219e2767c35c802d3e76eb
charming <464968841@qq.com>
2022-01-05 16:34:29 +0000

    partial pass lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,4 +1,5 @@
 #include "stream_reassembler.hh"
+
 #include <iostream>
 
 // Dummy implementation of a stream reassembler.
@@ -9,7 +10,7 @@
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -38,18 +39,12 @@ long StreamReassembler::merge_block(block_node &elm1, const block_node &elm2) {
     }
 }
 
-
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
-    // cout<<"input:"<< data <<endl;
-    // if (_unassembled_bytes.find(index) != _unassembled_bytes.end()) {
-    //     _unassembled_bytes.insert(pair<int , string>(index, data));
-    // }
     // _output.end_input
-    if (index > _head_index+_capacity) {
+    if (index > _head_index + _capacity) {
         return;
     }
     long merged_bytes = 0;
@@ -59,63 +54,96 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
     // size_t to_be_delete_size = 0;
     // 重复字符串，可以直接忽略
     if (index + data.length() <= _head_index) {
+        // 收到eof表示已经收到结束字符，只需让剩下的unassemb
+        if (eof) {
+            _eof_flag = true;
+        }
+        if (_eof_flag && empty()) {
+            _output.end_input();
+        }
         return;
-    } else if (index < _head_index) {  // 和起始点的重叠了，可以写入stream中，可以向后继续合并
-        size_t offset = _head_index - index; // substring的新起点
-        elm.data.assign(data.begin()+offset, data.end());
+    } else if (index <= _head_index) {  // 和起始点的重叠了，可以写入stream中，可以向后继续合并
+        size_t offset = _head_index - index;  // substring的新起点
+        elm.data.assign(data.begin() + offset, data.end());
         elm.begin = _head_index;
         elm.length = elm.data.length();
-       
-        // merge next
-        iter = _blocks.begin();
-        while ((merged_bytes = merge_block(elm, *iter)) >= 0) {
-            _unassembled_byte -= merged_bytes;
-            to_be_delete.push_back(iter);
-            iter ++;
+        // 如果此时_block 为空，则直接写入strem就行了
+        if (!_blocks.size() == 0) {
+            _unassembled_byte += elm.length;
+            // merge next
+            iter = _blocks.begin();
+            while ((merged_bytes = merge_block(elm, *iter)) >= 0) {
+                cout << "2222" << endl;
+                _unassembled_byte -= merged_bytes;
+                to_be_delete.push_back(iter);
+                iter++;
+            }
+            // 清理掉set中被合并的节点
+            for (size_t i = 0; i < to_be_delete.size(); i++) {
+                // to_be_delete_size += iter->length;
+                _blocks.erase(to_be_delete[i]);
+            }
+            // _unassembled_byte -= to_be_delete_size;
         }
-        // 清理掉set中被合并的节点
-        for (size_t i = 0; i < to_be_delete.size(); i++)
-        {   
-            // to_be_delete_size += iter->length;
-            _blocks.erase(to_be_delete[i]);
-        }
+
         // 把新合并的节点刷进stream
         _output.write(elm.data);
-        
-        // _unassembled_byte -= to_be_delete_size;
+        _head_index += elm.length;
+        // 收到eof表示已经收到结束字符，只需让剩下的unassemb
+        if (eof) {
+            _eof_flag = true;
+        }
+        if (_eof_flag && empty()) {
+            _output.end_input();
+        }
 
-    } else {   // 在起始字符串的后面一截，两者没有交集，没法合入到stream中，可以和前后的合并
+    } else {  // 在起始字符串的后面一截，两者没有交集，没法合入到stream中，可以和前后的合并
+        // 如果碰到eof，加上标记，
+        if (eof) {
+            _eof_flag = true;
+        }
+        // 如果长度为零直接不用插入了
+        if (data.size() == 0)
+        {
+            return;
+        }
+        
+        cout << "3333" << endl;
         elm.begin = index;
         elm.length = data.length();
         elm.data = data;
-        iter = _blocks.lower_bound(elm);
-        // merge before 
-        if (iter != _blocks.begin()) {
-            iter--;
-            if ((merged_bytes = merge_block(elm, *iter)) >= 0) {
+        _unassembled_byte += merged_bytes;
+        if (!_blocks.size() == 0) {
+            iter = _blocks.lower_bound(elm);
+            // merge before
+            if (iter != _blocks.begin()) {
+                iter--;
+                if ((merged_bytes = merge_block(elm, *iter)) >= 0) {
+                    _unassembled_byte -= merged_bytes;
+                    to_be_delete.push_back(iter);
+                }
+                iter++;
+            }
+
+            cout << "44444" << endl;
+            // merge back
+            while ((merged_bytes = merge_block(elm, *iter)) >= 0) {
+                cout << "111" << endl;
                 _unassembled_byte -= merged_bytes;
                 to_be_delete.push_back(iter);
+                iter++;
             }
-            iter++;
-        }
-        // merge back 
-        while ((merged_bytes = merge_block(elm, *iter)) >= 0) {
-            _unassembled_byte -= merged_bytes;
-            to_be_delete.push_back(iter);
-            iter ++;
-        }
 
-        // 清理掉set中被合并的节点
-        for (size_t i = 0; i < to_be_delete.size(); i++)
-        {   
-            // to_be_delete_size += iter->length;
-            _blocks.erase(to_be_delete[i]);
+            // 清理掉set中被合并的节点
+            for (size_t i = 0; i < to_be_delete.size(); i++) {
+                // to_be_delete_size += iter->length;
+                _blocks.erase(to_be_delete[i]);
+            }
         }
-        
         _blocks.insert(elm);
     }
 }
 
 size_t StreamReassembler::unassembled_bytes() const { return _unassembled_byte; }
 
-bool StreamReassembler::empty() const { return _unassembled_byte==0; }
+bool StreamReassembler::empty() const { return _unassembled_byte == 0; }
