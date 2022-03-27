168d7fa9808a295306e0410ebe2bdd72f2e610c6
ssssaaa111 <464968841@qq.com>
2022-03-27 16:11:52 +0000

    pass wrap && unwrap testcase

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -15,7 +15,9 @@ using namespace std;
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
     DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t largeNumber = n + isn.raw_value();
+    uint32_t smallNumber = largeNumber & 0xFFFFFFFF;
+    return WrappingInt32{smallNumber};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -30,5 +32,51 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     DUMMY_CODE(n, isn, checkpoint);
+    uint64_t prefix = checkpoint & 0xFFFFFFFF00000000;
+    uint64_t relate_sqn = 0;
+    if (n.raw_value() >= isn.raw_value())
+    {
+        relate_sqn = n.raw_value() - isn.raw_value();
+    } else {
+        relate_sqn = 0x100000000 - (isn.raw_value() - n.raw_value());
+    }
+    
+    // 没有突破32位
+    if (checkpoint < 0xFFFFFFFF) {
+        uint64_t right = relate_sqn +0x100000000;
+        if (checkpoint <= relate_sqn)
+        {
+            return relate_sqn;
+        }
+        
+        if ((checkpoint - relate_sqn) < (right - checkpoint))
+        {
+            return relate_sqn;
+        } else {
+            return right;   
+        }
+        
+        
+    } else
+    {
+        uint64_t conbain = prefix + relate_sqn;
+        if (conbain > checkpoint)
+        {
+            if (conbain - checkpoint < checkpoint - (conbain -0x100000000)) 
+            {
+                return conbain;
+            } else {
+                return conbain - 0x100000000;
+            }
+            
+        } else {
+            if (conbain + 0x100000000 - checkpoint < checkpoint - conbain) 
+            {
+                return conbain + 0x100000000;
+            } else {
+                return conbain;
+            }
+        }
+    }
     return {};
 }
